40cbff613ed36d3a8fdf026b21a8f4219baf406d
muathzoubi <moathaza@gmail.com>
2024-11-25 05:07:42 +0300

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD9mMwByHjqIz8iHaptNrvgQiPfqistnqo',
+  authDomain: 'inve-849b8.firebaseapp.com',
+  projectId: 'inve-849b8',
+  storageBucket: 'inve-849b8.appspot.com',
+  messagingSenderId: '247630962733',
+  appId: '1:247630962733:web:db285a97ce0d3b909998a4',
+  measurementId: 'G-L0FEEWVGYT',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
